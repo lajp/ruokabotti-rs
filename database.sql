e957aaa8b42c3970f9bde1ac07fb86f563e33b64
Luukas Pörtfors <lajp@iki.fi>
2021-11-01 22:00:43 +0200

    Use workflows for building

---
+++ database.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE RuokaDB;
+USE RuokaDB;
+
+CREATE TABLE Ruoat (
+	RuokaID int NOT NULL AUTO_INCREMENT,
+	RuokaName TINYTEXT NOT NULL,
+	ImageName TINYTEXT,
+	PRIMARY KEY (RuokaID)
+);
+
+CREATE TABLE Arvostelut (
+	RuokaID int NOT NULL,
+	KayttajaID varchar(18) NOT NULL,
+	Arvosana int NOT NULL,
+	FOREIGN KEY (RuokaID) REFERENCES Ruoat(RuokaID)
+);
+
+CREATE TABLE Ruokalista (
+	PVM DATE UNIQUE NOT NULL,
+	RuokaID int NOT NULL,
+	KokoRuoka TINYTEXT NOT NULL,
+	FOREIGN KEY (RuokaID) REFERENCES Ruoat(RUOKAID)
+);
