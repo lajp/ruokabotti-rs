50b5d65182d5d903749c67f82a7a8b5bb2f2e438
romeq <me@romeq.fi>
2022-01-30 23:23:40 +0200

    Push some nontested nonsense
    Reassigning `caps` so many times annoyed me so I decided to make this PR.
    I also moved `.await?`s to same row where their functions were.
    
    I have no damn idea if whole parser compiles as I didn't even try to compile (or test) it.

--- src/util/parse_lykeion.rs
+++ src/util/parse_lykeion.rs
@@ -13,24 +13,26 @@ pub async fn parse_lykeion() -> Result<Vec<(NaiveDate, String)>, anyhow::Error> 
     for i in 1..=7 {
         let resp = r_client
             .get(format!("https://aromimenu.cgisaas.fi/VaasaAromieMenus/fi-FI/Default/_/CampusLykeion/Rss.aspx?Id=0c9160c7-bedb-4b60-9ee1-188bf43a02b3&DateMode={}", i))
-            .send()
-            .await?
-            .bytes()
-            .await?;
+            .send().await?
+            .bytes().await?;
+        
         let channel = Channel::read_from(&resp[..])?;
+        
         for item in channel.items {
-            let date_str = item.title.unwrap();
-            let date_str = &date_str[date_str.find(' ').unwrap()+1..];
+            let date_str = &item.title.unwrap()[date_str.find(' ').unwrap()+1..];
             let date = NaiveDate::parse_from_str(date_str, "%d.%m.%Y").unwrap();
             let foodstr = item.description.unwrap();
-            let caps = FOOD_REGEX.captures(&foodstr).unwrap();
-            let caps = caps.get(1).unwrap().as_str();
-            let caps = ALLERGEENI_REGEX.replace_all(caps, ",");
-            let caps = caps.replace("M,G,V", "");
-            let caps = caps.replace("., ", "");
-            let caps = caps.replace(" ,", ",");
-            let caps = caps.replace(",,", ",");
-            let caps = caps.replace("  ", " ");
+            let caps = 
+            let caps = ALLERGEENI_REGEX.replace_all(FOOD_REGEX
+                .captures(&foodstr).unwrap()
+                .get(1).unwrap()
+                .as_str(), ",")
+                .replace("M,G,V", "")
+                .replace("., ", "")
+                .replace(" ,", ",")
+                .replace(",,", ",")
+                .replace("  ", " ");
+            
             retvec.push((date, caps.trim_end_matches(|c| c == ',' || c == ' ' || c == 'M').to_string()))
         }
     }
